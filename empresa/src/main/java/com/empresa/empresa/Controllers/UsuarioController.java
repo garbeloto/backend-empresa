1915abc9c3cfcab34bbde09a286cf09a1ec59ede
Rafaela Garbeloto <garbeloto21@gmail.com>
2025-10-28 21:46:31 -0300

    security funcionando e salvando com validações e senha criptografada

--- empresa/src/main/java/com/empresa/empresa/Controllers/UsuarioController.java
+++ empresa/src/main/java/com/empresa/empresa/Controllers/UsuarioController.java
@@ -1,6 +1,7 @@
 package com.empresa.empresa.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +18,32 @@ public class UsuarioController {
     @Autowired
     UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
     @PostMapping("/cadastrarUsuario")
     public String cadastrarUsuario (@RequestBody Usuario usuario){
         usuarioRepository.save(usuario);
 
         return "Usuário cadastrado com sucesso!";
     }
+
+    @PostMapping("/salvarComValidacoes")
+    public String salvarEmailUnico (@RequestBody Usuario usuario){ //verificar se o email já existe no banco de dados
+
+        String email = usuario.getEmailUsuario();
+
+        if (usuarioRepository.findByEmailUsuario(email).isPresent()) {
+            return "Email já cadastrado!";
+        } else {
+
+            String senhaCript = passwordEncoder.encode(usuario.getSenhaUsuario());
+            usuario.setSenhaUsuario(senhaCript);
+
+            usuarioRepository.save(usuario);
+            return "Pessoa salva com sucesso!";
+        }
     
+}
 }
--- empresa/src/main/java/com/empresa/empresa/Repositories/UsuarioRepository.java
+++ empresa/src/main/java/com/empresa/empresa/Repositories/UsuarioRepository.java
@@ -1,9 +1,11 @@
 package com.empresa.empresa.Repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.empresa.empresa.Entities.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
-
+    Optional<Usuario> findByEmailUsuario (String emailUsuario);
 }
